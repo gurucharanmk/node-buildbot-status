23250abf98098f6b8ce1e4dc068936790389ab47
Gurucharan MK <charan4u@gmail.com>
2016-07-25 06:47:40 -0400

    Added Airbnb eslint support and accordingly modified the gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-var excludeGitignore = require('gulp-exclude-gitignore');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const excludeGitignore = require('gulp-exclude-gitignore');
 
-var del = require('del');
-var babel = require('gulp-babel');
+const del = require('del');
+const babel = require('gulp-babel');
 
-var path = require('path');
-var nsp = require('gulp-nsp');
+const path = require('path');
+const nsp = require('gulp-nsp');
 
-var mocha = require('gulp-mocha');
-var util = require('gulp-util');
+const mocha = require('gulp-mocha');
+// const util = require('gulp-util');
 
-var istanbul = require('gulp-istanbul');
-var isparta = require('isparta');
+const istanbul = require('gulp-istanbul');
+const isparta = require('isparta');
 
 
 // Initialize the babel transpiler so ES2015/ES6 files gets compiled on runtime
-//Used only for development purpose
+// Used only for development purpose
 require('babel-register');
 
-gulp.task('lint', function () {
-  return gulp.src('**/*.js')
-    //ESLint ignores files with "node_modules" and other path mentioned in .gitignore files.
+gulp.task('lint', () => {
+  gulp.src('**/*.js')
+    // ESLint ignores files with "node_modules" and other path mentioned in .gitignore files.
     .pipe(excludeGitignore())
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
@@ -35,40 +35,40 @@ gulp.task('lint', function () {
 });
 
 
-gulp.task('babel', ['clean'], function () {
-  return gulp.src('lib/**/*.js')
+gulp.task('babel', ['clean'], () => {
+  gulp.src('lib/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('src'));
 });
 
-gulp.task('clean', function () {
-  return del('src');
+gulp.task('clean', () => {
+  del('src');
 });
 
-//gulp.task('test', function() {
+// gulp.task('test', function() {
 //    return gulp.src(['test/**/*.js'])
 //        .pipe(mocha({
 //            compilers: {
 //                js: babel
 //            }
 //        }));
-//});
+// });
 
 
-gulp.task('pre-test', function () {
-  return gulp.src(['lib/**/*.js'])
+gulp.task('pre-test', () => {
+  gulp.src(['lib/**/*.js'])
     .pipe(excludeGitignore())
     // Covering files
     .pipe(istanbul({
       includeUntested: true,
-      instrumenter: isparta.Instrumenter
+      instrumenter: isparta.Instrumenter,
     }))
     // Force `require` to return covered files
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function () {
-  return gulp.src(['test/*.js'])
+gulp.task('test', ['pre-test'], () => {
+  gulp.src(['test/*.js'])
     .pipe(mocha())
     // Creating the reports after tests ran
     .pipe(istanbul.writeReports())
@@ -76,18 +76,18 @@ gulp.task('test', ['pre-test'], function () {
     .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
 });
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
+gulp.task('nsp', (cb) => {
+  nsp({ package: path.resolve('package.json') }, cb);
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch(['lib/**/*.js', 'test/**'], ['lint']);
-    //gulp.watch('scss/*.scss', ['sass']);
+gulp.task('watch', () => {
+  gulp.watch(['lib/**/*.js', 'test/**'], ['lint']);
+  // gulp.watch('scss/*.scss', ['sass']);
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
 
-gulp.task('default', ['lint', 'test'], function () {
+gulp.task('default', ['lint', 'test'], () => {
     // This will only run if the lint task is successful...
 });
